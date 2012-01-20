ef81bd5333428ed3fb08a415fcf71901ceee3aea
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-01-20 10:51:52 -0500

    work now

--- deploy/adhoc.sh
+++ deploy/adhoc.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # Remember that this script will be executed by the unix user who push to the
 # git repository ; and the script will be executed in ~/www/codebase/.
 
 # Update requirements
-source /home/udbhav/adhoc-env/bin/activate
+. /home/udbhav/adhoc-env/bin/activate
 pip install -r ~/www/django/adhoc/requirements.txt
 python ~/www/django/adhoc/manage.py collectstatic
