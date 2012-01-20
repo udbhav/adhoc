c774f7fbe3aac8bb7ccf6869470c4f127b62625d
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-01-20 12:07:03 -0500

    added 404

--- deploy/adhoc.sh
+++ deploy/adhoc.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Remember that this script will be executed by the unix user who push to the
-# git repository ; and the script will be executed in ~/www/codebase/.
-
-# Update requirements
 . /home/udbhav/adhoc-env/bin/activate
 pip install -r ~/www/django/adhoc/requirements.txt
 python ~/www/django/adhoc/manage.py collectstatic --noinput
