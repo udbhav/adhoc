4123d522a1b8770ef8ec5f60f70e99a29b71eb6b
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-01-20 10:54:18 -0500

    will definitely work now

--- deploy/adhoc.sh
+++ deploy/adhoc.sh
@@ -5,4 +5,4 @@
 # Update requirements
 . /home/udbhav/adhoc-env/bin/activate
 pip install -r ~/www/django/adhoc/requirements.txt
-python ~/www/django/adhoc/manage.py collectstatic
+python ~/www/django/adhoc/manage.py collectstatic --noinput
